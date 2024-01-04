3dde2222c26f5492f4ad1e8eb044caee7fd083ad
Sahil Singh <sahilsingh70526@gmail.com>
2024-01-04 11:58:00 +0530

    issue fixed

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ io.on('connection', (socket) => {
     socket.emit('connected');
   });
   socket.on('join room', (room) => {
-    socket.join(room);
+    socket.join(room)
   });
   socket.on('typing', (room) => socket.in(room).emit('typing'));
   socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
